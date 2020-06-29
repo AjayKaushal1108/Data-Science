41cdd432e75dbb2150f6a5373e82c9bcc519ef83
Ajay Kumar Kaushal <49163487+ajay110597@users.noreply.github.com>
2020-06-29 22:21:32 +0530

    Update Assign.py.py

--- Assign.py.py
+++ Assign.py.py
@@ -4,14 +4,9 @@
 # ## This notebook will be mainly used for the capstone project.
 
 # In[5]:
-
-
 import pandas as pd
 import numpy as np
 
-
 # In[6]:
-
-
 print("Hello Capstone Project Course!")
 
